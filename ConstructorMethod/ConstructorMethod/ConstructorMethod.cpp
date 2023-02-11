cb5f67543521be0865fd7dba274d07f994f92bdd
NesAbosso <117785546+NesAbosso@users.noreply.github.com>
2023-02-10 20:16:23 -0700

    added

---
+++ ConstructorMethod/ConstructorMethod/ConstructorMethod.cpp
@@ -0,0 +1,39 @@
+// ConstructorMethod.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+
+class Rectangle {
+public:
+    int width, height;
+    Rectangle(int, int); // Constructor prototype
+};
+
+// class has a constructor with two parameters w and h
+// Constructor definition outside the class
+Rectangle::Rectangle(int w, int h) {
+    width = w;
+    height = h;
+}
+
+// Using the constructor to create an object
+int main() {
+    Rectangle rectangle(10, 20);
+    cout << "Width of the rectangle: " << rectangle.width << endl;
+    cout << "Height of the rectangle: " << rectangle.height << endl;
+    return 0;
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
